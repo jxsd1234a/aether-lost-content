7a0f153d83e9fa683be113dbcfe62ef24c268dc5
Bailey <wheresperry41@gmail.com>
2019-04-14 13:04:32 -0500

    Added the Gale slab

--- src/main/java/com/legacy/lostaether/BlocksLostAether.java
+++ src/main/java/com/legacy/lostaether/BlocksLostAether.java
@@ -3,11 +3,14 @@ package com.legacy.lostaether;
 import com.legacy.aether.blocks.BlockAetherSapling;
 import com.legacy.aether.blocks.decorative.BlockAetherStairs;
 import com.legacy.aether.blocks.decorative.BlockAetherWall;
+import com.legacy.aether.items.block.ItemAetherSlab;
 import com.legacy.aether.registry.creative_tabs.AetherCreativeTabs;
 import com.legacy.lostaether.blocks.BlockLostDungeonBase;
+import com.legacy.lostaether.blocks.BlockLostSlab;
 import com.legacy.lostaether.world.AetherGenCrystalTree;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockSlab;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -48,6 +51,9 @@ public class BlocksLostAether
 		locked_gale_stone = register("locked_gale_stone", new BlockLostDungeonBase(true)).setCreativeTab(null);
 		locked_light_gale_stone = register("locked_light_gale_stone", new BlockLostDungeonBase(true)).setCreativeTab(null);
 		
+		gale_double_slab = register("gale_double_slab", new BlockLostSlab("gale_double_slab", true, Material.ROCK).setHardness(2.0F).setResistance(10.0F)).setCreativeTab(null);
+		gale_slab = registerSlab("gale_slab", new BlockLostSlab("gale_slab", false, Material.ROCK).setHardness(0.5F).setResistance(10.0F), gale_double_slab);
+		
 		gale_stairs = register("gale_stairs", new BlockAetherStairs(gale_stone.getDefaultState()));
 		
 		gale_wall = register("gale_wall", new BlockAetherWall(gale_stone.getDefaultState()));
@@ -81,5 +87,16 @@ public class BlocksLostAether
 		block.setCreativeTab(AetherCreativeTabs.blocks);
 
 		return block;
+	}
+	
+	public static Block registerSlab(String name, Block singleSlab, Block doubleSlab)
+	{
+		singleSlab.setCreativeTab(AetherCreativeTabs.blocks);
+		doubleSlab.setCreativeTab(null);
+
+		iBlockRegistry.register(singleSlab.setRegistryName(LostAetherContent.locate(name)));
+		iItemRegistry.register(new ItemAetherSlab(singleSlab, (BlockSlab) singleSlab, (BlockSlab) doubleSlab).setRegistryName(LostAetherContent.locate(name)));
+
+		return singleSlab;
 	}
 }
