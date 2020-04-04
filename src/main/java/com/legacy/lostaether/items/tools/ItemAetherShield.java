96e5c16b2c97fc6fb7899a03013136af425ec202
Bailey <wheresperry41@gmail.com>
2020-04-03 22:00:23 -0500

    Reworked shields.

--- src/main/java/com/legacy/lostaether/items/tools/ItemAetherShield.java
+++ src/main/java/com/legacy/lostaether/items/tools/ItemAetherShield.java
@@ -9,77 +9,75 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.IItemPropertyGetter;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemShield;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemAetherShield extends Item
+@SuppressWarnings("deprecation")
+public class ItemAetherShield extends ItemShield
 {
-    public ItemAetherShield()
-    {
-        this.maxStackSize = 1;
-        this.setCreativeTab(AetherCreativeTabs.weapons);
-        this.setMaxDamage(300);
-        this.addPropertyOverride(new ResourceLocation("blocking"), new IItemPropertyGetter()
-        {
-            @SideOnly(Side.CLIENT)
-            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
-            {
-                return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
-            }
-        });
-        BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(this, ItemArmor.DISPENSER_BEHAVIOR);
-    }
+	public ItemAetherShield()
+	{
+		this.maxStackSize = 1;
+		this.setCreativeTab(AetherCreativeTabs.weapons);
+		this.setMaxDamage(300);
+		this.addPropertyOverride(new ResourceLocation("blocking"), new IItemPropertyGetter()
+		{
+			@SideOnly(Side.CLIENT)
+			public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+			{
+				return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
+			}
+		});
+		BlockDispenser.DISPENSE_BEHAVIOR_REGISTRY.putObject(this, ItemArmor.DISPENSER_BEHAVIOR);
+	}
 
-    /**
-     * returns the action that specifies what animation to play when the items is being used
-     */
-    public EnumAction getItemUseAction(ItemStack stack)
-    {
-        return EnumAction.BLOCK;
-    }
+	@Override
+	public String getItemStackDisplayName(ItemStack stack)
+	{
+		return I18n.translateToLocal(this.getUnlocalizedNameInefficiently(stack) + ".name").trim();
+	}
 
-    /**
-     * How long it takes to use or consume an item
-     */
-    public int getMaxItemUseDuration(ItemStack stack)
-    {
-        return 72000;
-    }
+	@Override
+	public EnumAction getItemUseAction(ItemStack stack)
+	{
+		return EnumAction.BLOCK;
+	}
 
-    /**
-     * Called when the equipped item is right clicked.
-     */
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
-    {
-    	ItemStack itemstack = playerIn.getHeldItem(handIn);
-    	
-    	if (playerIn.getCooldownTracker().hasCooldown(itemstack.getItem()))
-    	{
-    		return new ActionResult<ItemStack>(EnumActionResult.FAIL, itemstack);
-    	}
-    	else
-    	{
-    		playerIn.setActiveHand(handIn);
-    		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
-    	}
-    }
+	@Override
+	public int getMaxItemUseDuration(ItemStack stack)
+	{
+		return 72000;
+	}
 
-    /**
-     * Return whether this item is repairable in an anvil.
-     *  
-     * @param toRepair the {@code ItemStack} being repaired
-     * @param repair the {@code ItemStack} being used to perform the repair
-     */
-    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair)
-    {
-        return false; //repair.getItem() == Item.getItemFromBlock(Blocks.PLANKS) ? true : super.getIsRepairable(toRepair, repair);
-    }
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+	{
+		ItemStack itemstack = playerIn.getHeldItem(handIn);
+
+		if (playerIn.getCooldownTracker().hasCooldown(itemstack.getItem()))
+		{
+			return new ActionResult<ItemStack>(EnumActionResult.FAIL, itemstack);
+		}
+		else
+		{
+			playerIn.setActiveHand(handIn);
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+		}
+	}
+
+	@Override
+	public boolean getIsRepairable(ItemStack toRepair, ItemStack repair)
+	{
+		return false; // repair.getItem() == Item.getItemFromBlock(Blocks.PLANKS) ? true :
+						// super.getIsRepairable(toRepair, repair);
+	}
 }
